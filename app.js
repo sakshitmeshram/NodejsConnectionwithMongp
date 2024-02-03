3f047221051f14d69d5eafc20805bd2fa6235699
sakshitmeshram <sakshitmeshram2001@gmail.com>
2024-02-03 15:08:18 +0530

    Connect node with Mongodb

--- app.js
+++ app.js
@@ -9,20 +9,6 @@ const insertData = async () => {
     console.log(result);
 }
 
-// function insert async () => {
-
-//     collection.insertOne(
-//      ,
-//         (err, result) => {
-//             if (err) {
-//                 console.error('Error inserting document:', err);
-//                 return;
-//             }
-//             console.log('Document inserted:', result.insertedId);
-//         }
-//     );
-// }
-
 insertData();
 
 
